ddc61fa038edf7ba13e840c66f00987fa0ac63fb
Swift <swift@valter.local>
2017-02-14 22:42:37 -0200

    Campos básicos

---
+++ S400_Aula02_CadastroSQLite/S400_Aula02_CadastroSQLite-Bridging-Header.h
@@ -0,0 +1,5 @@
+//
+// Criar um arquivo vazio de Objective-C e nao esquecer de criar um briding
+// para um arquivo com extensao .h
+//
+#import <sqlite3.h>
--- S400_Aula02_CadastroSQLite/ViewController.swift
+++ S400_Aula02_CadastroSQLite/ViewController.swift
@@ -1,25 +1,107 @@
-//
-//  ViewController.swift
-//  S400_Aula02_CadastroSQLite
-//
-//  Created by Swift on 14/02/17.
-//  Copyright © 2017 Swift. All rights reserved.
-//
 
 import UIKit
 
+//MARK: Sandbox
+let caminhoParaSandbox = NSHomeDirectory()
+let caminhoDocuments = (caminhoParaSandbox as NSString).appendingPathComponent("Documents")
+let caminhoArquivo = (caminhoDocuments as NSString).appendingPathComponent("arquivo.sqlite")
+
+
 class ViewController: UIViewController {
+    
+    //MARK: Outlets
+    @IBOutlet weak var textFieldNome: UITextField!
+    @IBOutlet weak var textFieldIdade: UITextField!
+    @IBOutlet weak var textViewLista: UITextView!
+    
+    //MARK: Propriedades
+    var database : OpaquePointer? = nil //Objeto que ajuda a escrever códigos de C em Swift
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Verificando se o arquivo do banco existe
+        if (FileManager.default.fileExists(atPath: caminhoArquivo)){
+            
+            //Caso o banco exista
+            if (sqlite3_open(caminhoArquivo, &database) == SQLITE_OK){
+                
+            }
+            
+            //Caso o banco tenha sido aberto
+            print("Banco de dados aberto com sucesso")
+            
+            
+        } else {
+            
+            print("Erro ao abrir o banco")
+        
+        }
+        
+        
+        //
+
+    }
+    
+    //MARK: Actions
+    @IBAction func cadastrarAluno(_ sender: UIButton) {
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func listarAlunos(_ sender: UIButton) {
     }
-
+    
+    //MARK: Métodos de UIResponder
+    override var canBecomeFirstResponder: Bool{
+        return true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.becomeFirstResponder()
+    }
 
 }
+
+//MARK: Extension de String
+extension String{
+    var intValor : Int{
+        let conversao = self as NSString
+        return Int(conversao.intValue)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
